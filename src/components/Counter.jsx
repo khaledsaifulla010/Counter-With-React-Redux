6d1a1b39e9072fc72a5e406c1c9add8640029a49
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-29 08:03:39 +0600

    added increment & decrement functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-
+import { decrement, increment } from "../redux/features/counter/counterSlice";
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
@@ -7,11 +7,17 @@ const Counter = () => {
   return (
     <div>
       <div className="flex items-center justify-center border border-purple-700 py-10 w-[600px] mx-auto mt-24 gap-10 rounded-md">
-        <button className="text-lg font-bold text-green-800 border border-green-300 bg-green-100 p-2 rounded-md">
+        <button
+          onClick={() => dispatch(increment())}
+          className="text-lg font-bold text-green-800 border border-green-300 bg-green-100 p-2 rounded-md"
+        >
           Increment
         </button>
-        <h1 className="text-5xl font-bold">0</h1>
-        <button className="text-lg font-bold text-red-800 border border-red-300 bg-red-100 p-2 rounded-md">
+        <h1 className="text-5xl font-bold">{count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="text-lg font-bold text-red-800 border border-red-300 bg-red-100 p-2 rounded-md"
+        >
           Decrement
         </button>
       </div>
