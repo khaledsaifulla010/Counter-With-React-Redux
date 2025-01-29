0b960118b5cb0d103827fb7e3ce8bd493aac6509
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-29 07:36:19 +0600

    setup projects environment

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-purple-800 text-center mt-20 font-bold">
+        Counter With React-Redux
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
